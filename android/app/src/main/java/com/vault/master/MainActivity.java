7d60f38afcc6f9c69adec8976d46ce23e34adf60
Julio <julio@example.com>
2026-01-28 19:35:50 -0300

    Initial commit: Translated and fixed version

---
+++ android/app/src/main/java/com/vault/master/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vault.master;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    },
+    title: "Vault-Master Secure",
+    icon: path.join(__dirname, 'icon.ico')
+  });
+
+  win.loadFile('www/index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
